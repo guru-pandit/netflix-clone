d0157a0ab0a96217b56daeaab060b9a040aa16a2
Guruprasad Pandit <gurupandit28@gmail.com>
2021-01-03 18:46:56 +0530

    Rows Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import Row from "./components/Row";
+import requests from "./request";
 
 function App() {
   return (
     <div className="app">
-      <h1>App Page</h1>
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentories} />
     </div>
   );
 }
--- src/request.js
+++ src/request.js
@@ -1,12 +1,14 @@
+const API_KEY = `f6314cabee93097419f6f19f2e4c341c`;
+
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}&language=en-us`,
-  fetchNetflixOriginals: `/discover/tv?${process.env.REACT_APP_API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-us`,
-  fetchActionMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=104749`,
-  fetchDocumentories: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=99`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-us`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=104749`,
+  fetchDocumentories: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 
 export default requests;
